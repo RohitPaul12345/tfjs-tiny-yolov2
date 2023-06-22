4a19216253960b3571bb49d335a11d7ac566b3c8
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-06-22 11:56:11 +0530

    Update index.js

--- build/commonjs/index.js
+++ build/commonjs/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-var tf = require("@tensorflow/tfjs-core");
+let tslib_1 = require("tslib");
+let tf = require("@tensorflow/tfjs-core");
 exports.tf = tf;
 tslib_1.__exportStar(require("tfjs-image-recognition-base"), exports);
 tslib_1.__exportStar(require("./common"), exports);
